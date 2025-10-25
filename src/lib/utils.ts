a25b15f0125b0d97034af429997bcad3395cd666
Elia <elia.zarantonello97@gmail.com>
2025-10-25 15:53:21 +0200

    Created serializer function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,8 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
+const ISO_DATE_REGEX = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?Z?$/;
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
@@ -10,4 +12,29 @@ export function formatDate(date: Date) {
     dateStyle: "medium",
     timeStyle: "short",
   }).format(new Date(date));
+}
+
+export function serializeData<T extends Record<string, unknown>>(
+  items: T[],
+): T[] {
+  return items.map((item) => {
+    const serialized: { [key: string]: unknown } = { ...item };
+    for (const key in serialized) {
+      const value = serialized[key];
+      if (
+        typeof value === "string" &&
+        !isNaN(Number(value)) &&
+        value.trim() !== ""
+      ) {
+        serialized[key] = Number(value);
+      } else if (
+        typeof value === "string" &&
+        !isNaN(Date.parse(value)) &&
+        ISO_DATE_REGEX.test(value)
+      ) {
+        serialized[key] = new Date(value).toISOString();
+      }
+    }
+    return serialized as T;
+  });
 }
