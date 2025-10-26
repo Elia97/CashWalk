4d2213c00474bb94fb7e557b701655aa73f2c888
Elia <elia.zarantonello97@gmail.com>
2025-10-26 22:33:56 +0100

    Updated utils functions

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,40 +1,46 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
-
-const ISO_DATE_REGEX = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}(\.\d+)?Z?$/;
+import { PiggyBank, Wallet } from "lucide-react";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
+export function formatCurrency(amount: number, currency: string) {
+  return new Intl.NumberFormat("it-IT", {
+    style: "currency",
+    currency,
+  }).format(amount);
+}
+
 export function formatDate(date: Date) {
-  return new Intl.DateTimeFormat(undefined, {
+  return new Intl.DateTimeFormat("it-IT", {
     dateStyle: "medium",
-    timeStyle: "short",
   }).format(new Date(date));
 }
 
-export function serializeData<T extends Record<string, unknown>>(
-  items: T[],
-): T[] {
-  return items.map((item) => {
-    const serialized: { [key: string]: unknown } = { ...item };
-    for (const key in serialized) {
-      const value = serialized[key];
-      if (
-        typeof value === "string" &&
-        !isNaN(Number(value)) &&
-        value.trim() !== ""
-      ) {
-        serialized[key] = Number(value);
-      } else if (
-        typeof value === "string" &&
-        !isNaN(Date.parse(value)) &&
-        ISO_DATE_REGEX.test(value)
-      ) {
-        serialized[key] = new Date(value).toISOString();
-      }
-    }
-    return serialized as T;
-  });
-}
+export const getAccountIcon = (type: string) => {
+  switch (type) {
+    case "checking":
+      return Wallet;
+    case "cash":
+      return PiggyBank;
+    case "savings":
+      return Wallet;
+    default:
+      return Wallet;
+  }
+};
+
+export const getAccountTypeLabel = (type: string) => {
+  switch (type) {
+    case "checking":
+      return "Checking Account";
+    case "cash":
+      return "Cash Account";
+    case "savings":
+      return "Savings Account";
+    default:
+      return "Unknown Account";
+  }
+};
